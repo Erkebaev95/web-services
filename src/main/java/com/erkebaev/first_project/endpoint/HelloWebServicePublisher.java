4268f153eb3d342bb910584b4d3a749085da14f9
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2022-12-12 16:21:58 +0600

    add

---
+++ src/main/java/com/erkebaev/first_project/endpoint/HelloWebServicePublisher.java
@@ -0,0 +1,16 @@
+package com.erkebaev.first_project.endpoint;
+
+// класс нашего веб-сервиса
+import com.erkebaev.first_project.HelloWebServiceImpl;
+
+// класс, для запуска веб-сервера с веб-сервисами
+import javax.xml.ws.Endpoint;
+
+public class HelloWebServicePublisher {
+    public static void main(String... args) {
+        // запускаем веб-сервер на порту 1986
+        // и по адресу, указанному в первом аргументе,
+        // запускаем веб-сервис, передаваемый во втором аргументе
+        Endpoint.publish("http://localhost:1986/wss/hello", new HelloWebServiceImpl());
+    }
+}
